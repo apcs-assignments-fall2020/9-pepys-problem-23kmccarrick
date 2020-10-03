9e0bdea5e94277134881194f342a6ead1e4fc7d0
23kmccarrick <23kmccarrick@athenian.org>
2020-10-03 01:35:36 +0000

    completed

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,22 +5,47 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int count = 0;
+        for(int i = 0; i < 10000; i ++){
+            if((int)(Math.random() * 6 + 1)==6||(int)(Math.random() * 6 + 1)==6||(int)(Math.random() * 6 + 1)==6||(int)(Math.random() * 6 + 1)==6||(int)(Math.random() * 6 + 1)==6||(int)(Math.random() * 6 + 1)==6)
+                count++;
+        }
+        return (double)count/(double)(10000)*100;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int count = 0;
+        int j = 0;
+        for(int i = 0; i < 10000; i ++){
+            for(int b = 0; b < 12; b++){
+                if((int)(Math.random() * 6 + 1)==6)
+                    j++;
+            }
+            if(j>1)
+                count++;
+            j = 0;
+        }
+        return (double)count/(double)(10000)*100;
     }
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int count = 0;
+        int j = 0;
+        for(int i = 0; i < 10000; i ++){
+            for(int b = 0; b < 18; b++){
+                if((int)(Math.random() * 6 + 1)==6)
+                    j++;
+            }
+            if(j>2)
+                count++;
+            j = 0;
+        }
+        return (double)count/(double)(10000)*100;
+     
     }
 
 
